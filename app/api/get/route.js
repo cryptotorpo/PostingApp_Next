8841f67d4c9dd8cee10c2e4cc4d1ddb77c6c394a
Air128731 <henry.scott0x8080@gmail.com>
2023-11-22 03:05:01 -0500

    update api/get

--- app/api/get/route.js
+++ app/api/get/route.js
@@ -3,7 +3,11 @@ import { NextResponse } from "next/server";
 const Model = require("../../models/model");
 
 export async function GET(req, res) {
-  await connect();
-  const data = await Model.find();
-  return NextResponse.json({ status: 200, data });
+  await connect().then(async () => {
+    const data = await Model.find();
+    return NextResponse.json({ status: 200, data });
+  }).catch(() => {
+    console.log('error')
+  })
+  return NextResponse.json({status: 200, data: []})
 }
